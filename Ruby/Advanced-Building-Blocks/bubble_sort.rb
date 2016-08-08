40c036707f51d3f29168476e11022eee361ef8e3
DidsyTurbo <jacklevideegan@gmail.com>
2016-08-08 15:34:23 +0100

    Create bubble_sort.rb
    Didn't want to reuse code for "#bubble_sort_by" so came up with a solution to just pass "#bubble_sort_by" a block inside "#bubble_sort" and use a <=> b to compare, but for the sake of the exercise I thought inheritance was a bit much, so I just left it the way I had originally worked through the problem.

---
+++ Ruby/Advanced-Building-Blocks/bubble_sort.rb
@@ -0,0 +1,26 @@
+def bubble_sort(array)
+	(1...array.size).each do |a|
+		(0...array.size - a).each do |b|
+			if array[b] > array[b + 1]
+				array[b], array[b + 1] = array[b + 1], array[b]
+			end 
+		end
+	end
+	array
+end
+
+def bubble_sort_by(array)
+	(1...array.size).each do |a|
+		(0...array.size - a).each do |b|
+			if yield(array[b], array[b + 1]) > 0
+				array[b], array[b + 1] = array[b + 1], array[b]
+			end
+		end
+	end
+	array
+end
+
+bubble_sort([4,3,78,2,0,2])
+
+bubble_sort_by(["hi","hello","hey"]) {|left, right| left.length - right.length}
+
